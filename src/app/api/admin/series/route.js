6150b8b4d26b0abb43ae1effec17ad767f0341fe
FERDIZ-afk <ferdizakyla@gmail.com>
2025-05-05 06:39:37 +0700

    千夜未来

---
+++ src/app/api/admin/series/route.js
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/route";
+import prisma from "@/lib/prisma";
+
+
+
+
+export async function GET(request) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    // Cek apakah pengguna login
+    if (!session || !session.user) {
+      return NextResponse.json(
+        { error: "Anda harus login untuk menambahkan komentar" },
+        { status: 401 }
+      );
+    }
+
+  
+
+    return NextResponse.json(session);
+  } catch (error) {
+    console.error("Error creating comment:", error);
+    return NextResponse.json(
+      { error: "Terjadi kesalahan saat menambahkan komentar" },
+      { status: 500 }
+    );
+  }
+}
